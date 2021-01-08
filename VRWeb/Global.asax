980a414aaa759092d2bd53627da714b392823ce2
Andi Leka <andigeri8@gmail.com>
2021-01-08 21:41:01 +0100

    Ye

---
+++ VRWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VRWeb.MvcApplication" Language="C#" %>
---
+++ VRWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VRWeb.Startup))]
+namespace VRWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
